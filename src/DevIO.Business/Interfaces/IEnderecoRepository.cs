81454dee1f7e29b4f93f800100be9c5414df4ea5
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-07-22 19:12:35 -0300

    Criando interfaces de repositorios

---
+++ src/DevIO.Business/Interfaces/IEnderecoRepository.cs
@@ -0,0 +1,14 @@
+﻿using DevIO.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Interfaces
+{
+	public interface IEnderecoRepository: IRepository<Fornecedor>
+	{
+		Task<Fornecedor> ObterEnderecoPorFornecedor(Guid fornecedorId);
+		Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid Id);
+	}
+}
