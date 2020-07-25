3d28d37cbea227be8651eb3e05a4d5068c0d8bc8
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-07-25 19:07:30 -0300

    Utilizando Modal Window de edição

--- src/Dev.IO.App/wwwroot/js/site.js
+++ src/Dev.IO.App/wwwroot/js/site.js
@@ -1,4 +1,42 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function AjaxModal() {
+	$(document).ready(function () {
+		$(function () {
+			$.ajaxSetup({ cache: false });
 
-// Write your JavaScript code.
+			$("a[data-modal]").on("click",
+				function (e) {
+					e.preventDefault();
+					$("#myModalContent").load(this.href,
+						function () {
+							$("#myModal").modal({
+								keyboard: true
+							},'show');
+							bindForm(this);
+						}
+					);
+				}
+			);
+		});
+
+		function bindForm(dialog) {
+			$('form', dialog).submit(function () {
+				$.ajax({
+					url: this.action,
+					type: this.method,
+
+					data: $(this).serialize(),
+					success: function (result) {
+						if (result.success) {
+							$('#myModal').modal('hide');
+							$('#EnderecoTarget').load(result.url);
+						} else {
+							$('#myModalContent').html(result);
+							bindForm(dialog);
+						}
+					}
+				});
+				return false;
+			});
+		}
+	});
+}
